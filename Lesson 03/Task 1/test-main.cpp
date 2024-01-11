ae2d304cf85165e373d1adb4d842b5f9867d19a7
walterXXX <icsystems@mail.ru>
2024-01-11 20:35:56 +0300

    Add Lesson 03 Task 1, Task 2

---
+++ Lesson 03/Task 1/test-main.cpp
@@ -0,0 +1,61 @@
+﻿#pragma once
+
+#include "catch2/catch_test_macros.hpp"
+#include "catch2/catch_session.hpp"
+#include "List.cpp"
+
+
+TEST_CASE("Class: List. Test method: bool Empty()", "[List.empty]") {
+
+	List testList; //создали тестовый список
+	
+	SECTION("for empty list") {
+		INFO("Тестовый список пока что пуст");
+		CHECK(testList.Empty() == true);
+	}
+
+	SECTION("for not empty list") {
+		testList.PushBack(10); // добавим элемент в список
+		INFO("В тестовый список не пустой");
+		CHECK(testList.Empty() == false);
+	}
+
+}
+
+TEST_CASE("Class: List. Test method: unsigned long Size()", "List.size") {
+
+	List testList; //создали тестовый список
+
+	SECTION("for empty list") {
+		INFO("Тестовый список пока что пуст");
+		CHECK(testList.Size() == 0);
+	}
+
+	SECTION("for not empty list") {
+		testList.PushBack(10); // добавим элемент в список
+		INFO("Размер списка не соответствует кол-ву добавленных элементов");
+		REQUIRE(testList.Size() == 1);
+		testList.PushBack(20);
+		REQUIRE(testList.Size() == 2);
+		testList.PushFront(50);
+		CHECK(testList.Size() == 3);
+	}
+
+}
+
+TEST_CASE("Class: List. Test method: void Clear()","List.clear") {
+
+	List testList; //создали тестовый список
+	testList.PushBack(10); // добавим элемент в список
+	testList.PushFront(40); // добавим элемент в список
+	testList.Clear();
+
+	INFO("Размер списка не обнулился после очистки");
+	CHECK(testList.Size() == 0);
+
+}
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	return Catch::Session().run();
+}
---
+++ Lesson 03/Task 2/test-main.cpp
@@ -0,0 +1,96 @@
+﻿#pragma once
+
+#include "catch2/catch_test_macros.hpp"
+#include "catch2/catch_session.hpp"
+#include "List.cpp"
+
+
+TEST_CASE("Class: List. Basic tests", "[basic_tests]") {
+
+	List testList; //создали тестовый список
+	int testBackVal = 10; // тестовое значение величины в конце списка
+	testList.PushBack(testBackVal);
+	
+	SECTION("PushBack method") {
+		INFO("В тестовый список был добавлен один элемент");
+		CHECK(testList.Empty() == false);
+		CHECK(testList.Size() == 1);
+	}
+
+	int testFrontVal = 20; // тестовое значение величины в начале списка
+	testList.PushFront(testFrontVal);
+
+	SECTION("PushFront method") {
+		INFO("В тестовый список было добавлено два элемента");
+		CHECK(testList.Empty() == false);
+		CHECK(testList.Size() == 2);
+	}
+
+	SECTION("PopBack method") {
+		INFO("Ошибка при извлечении в конце списка");
+		CHECK(testList.PopBack() == testBackVal);
+		CHECK(testList.PopBack() == testFrontVal);
+		REQUIRE(testList.Size() == 0);
+		// если список пуст, пробуем вызвать метод PopBack и тестируем корректную обработку исключения
+		try {
+			testList.PopBack();
+		}
+		catch (const std::runtime_error& e) {
+			CHECK(!std::strcmp(e.what(), "list is empty"));
+		}
+		
+	}
+
+	SECTION("PopFront method") {
+		INFO("Ошибка при извлечении в начале списка");
+		CHECK(testList.PopFront() == testFrontVal);
+		CHECK(testList.PopFront() == testBackVal);
+		REQUIRE(testList.Size() == 0);
+		// если список пуст, пробуем вызвать метод PopFront и тестируем корректную обработку исключения
+		try {
+			testList.PopFront();
+		}
+		catch (const std::runtime_error& e) {
+			CHECK(!std::strcmp(e.what(), "list is empty"));
+		}
+	}
+
+}
+
+TEST_CASE("Class: List. Advanced tests", "[advanced_tests]") {
+	// тест по сценарию: добавляем три элемента в начало списка, считываем их с конца и наоборот
+
+	List testList; //создали тестовый список
+	int t1 = 10; int t2 = 20; int t3 = 30; // тестовые значения
+
+	SECTION("PushFront/PopBack test", "[PushFornt_PopBack]") {
+		INFO("Ошибка в сценарии добавления элементов в начало списка, а считываня с конца");
+		testList.PushFront(t1);
+		testList.PushFront(t2);
+		testList.PushFront(t3);
+		CHECK(testList.Size() == 3);
+		CHECK(testList.PopBack() == t1);
+		CHECK(testList.PopBack() == t2);
+		CHECK(testList.PopBack() == t3);
+		CHECK(testList.Size() == 0);
+	}
+
+	SECTION("PushBack/PopFront test", "[PushBack_PopFront]") {
+		INFO("Ошибка в сценарии добавления элементов в конец списка, а считываня с начала");
+		testList.PushBack(t1);
+		testList.PushBack(t2);
+		testList.PushBack(t3);
+		CHECK(testList.Size() == 3);
+		CHECK(testList.PopBack() == t3);
+		CHECK(testList.PopBack() == t2);
+		CHECK(testList.PopBack() == t1);
+		CHECK(testList.Size() == 0);
+	}
+
+}
+
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	return Catch::Session().run();
+}
