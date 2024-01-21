44b7b48f38365deeab485035dc5ecb243cbc1ed3
walterXXXXX <icsystems@mail.ru>
2024-01-21 15:46:23 +0300

    Add Lesson 08. Task 1, task 2

---
+++ Lesson 08/Task 1/main.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+
+int main() {
+
+	std::cout << "[IN]: ";
+	std::string inputStr;
+	std::getline(std::cin, inputStr);
+	std::istringstream iss(inputStr);
+
+	std::vector<int> vec;
+	int value;
+	while (iss >> value)
+		vec.push_back(value);
+	
+	std::sort(vec.begin(), vec.end());
+	auto it = std::unique(vec.begin(), vec.end());
+	vec.erase(it, vec.end());
+
+	for (int c : vec)
+		std::cout << c << " ";
+
+	return 0;
+}
---
+++ Lesson 08/Task 2/main.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <set>
+#include <list>
+#include <vector>
+
+template <class T>
+void printContainer(T cont) {
+	for (std::string el : cont)
+		std::cout << el << " ";
+	std::cout << std::endl;
+}
+
+int main() {
+
+	std::set<std::string> test_set = { "one", "two", "three", "four" };
+	printContainer(test_set); // four one three two
+
+	std::list<std::string> test_list = { "one", "two", "three", "four" };
+	printContainer(test_list); // one, two, three, four
+
+	std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+	printContainer(test_vector); // one, two, three, four
+	
+	return 0;
+}
