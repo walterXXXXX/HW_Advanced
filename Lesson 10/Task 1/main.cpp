f665bb7452da77c0266a0896643917a2b1d92cc3
walterXXXXX <icsystems@mail.ru>
2024-01-27 00:21:21 +0300

    Add Lesson 10 Task1,2,3

---
+++ Lesson 10/Task 1/main.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+#include <vector>
+#include <memory>
+
+class tridiagonal_matrix {
+public:
+    std::vector<double> m_down;
+    std::vector<double> m_upper;
+    std::vector<double> m_middle;
+    tridiagonal_matrix(
+        const std::vector<double>& down,
+        const std::vector<double>& upper,
+        const std::vector<double>& middle) :
+        m_down{ down }, m_upper{ upper }, m_middle{ middle }
+    {};
+
+    std::unique_ptr<tridiagonal_matrix> clone() {
+        return std::make_unique<tridiagonal_matrix>(m_down, m_upper, m_middle);
+    }
+
+    void print() {
+        for (int i = 0; i < m_middle.size(); i++) {
+            for (int j = 0; j < m_middle.size(); j++) {
+                if (i == j) {
+                    std::cout << m_middle[j] << "\t";
+                    continue;
+                }           
+                if (j == i + 1) {
+                    std::cout << m_upper[i] << "\t";
+                    continue;
+                }
+                if (j == i - 1) {
+                    std::cout << m_down[j] << "\t";
+                    continue;
+                }
+                std::cout << "0" << "\t";
+            }
+            std::cout << std::endl;
+        }
+    }
+
+    ~tridiagonal_matrix() { std::cout << "destructor called\n"; }
+
+};
+
+int main() {
+    auto down = std::vector<double>{ -4.0, 5.0 };
+    auto upper = std::vector<double>{ 14.0, 8.0 };
+    auto middle = std::vector<double>{ 3.0, 1.0, 7.0 };
+    auto matrix = std::make_unique<tridiagonal_matrix>(
+        down,
+        upper,
+        middle
+    );
+
+    auto matrix_clone = matrix->clone();
+    
+    std::cout << "Base matrix: " << std::endl;
+    matrix->print();
+
+    std::cout << std::endl;
+    std::cout << "Clone matrix: " << std::endl;
+
+    matrix_clone->print();
+
+    return 0;
+}
---
+++ Lesson 10/Task 3/main.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+
+template <class T>
+class my_unique_ptr {
+public:
+	my_unique_ptr(T* ptr) :
+		_ptr(ptr) {};
+
+	my_unique_ptr(const my_unique_ptr&) = delete;
+
+	T& operator = (const my_unique_ptr&) = delete;
+
+	T& operator * () const {
+		return *_ptr;
+	}
+
+	T* operator -> () {
+		return _ptr;
+	}
+
+	T* release() {
+		T* temp = _ptr;
+		_ptr = nullptr;
+		return temp;
+	}
+
+	~my_unique_ptr() {
+		delete _ptr;
+	}
+
+private:
+	T* _ptr;
+};
+
+class MyClass {
+public:
+	int _a;
+	std::string _str;
+	
+	MyClass(int a, std::string str) :
+		_a(a), _str(str) {};
+
+	~MyClass(){
+		std::cout << "Called destructor";
+	}
+};
+
+
+int main() {
+
+	my_unique_ptr<MyClass> ptr1(new MyClass(1, "Test1"));
+	std::cout << (*ptr1)._a << "\t" << (*ptr1)._str << std::endl;
+
+	ptr1->_a = 2; ptr1->_str = "Test2";
+	std::cout << (*ptr1)._a << "\t" << (*ptr1)._str << std::endl;
+
+	MyClass* releasePtr = ptr1.release();
+	std::cout << (*releasePtr)._a << "\t" << (*releasePtr)._str << std::endl;
+
+	delete releasePtr;
+
+	return 0;
+}
